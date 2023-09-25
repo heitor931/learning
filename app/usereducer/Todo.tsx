0d941cce9594b9dab61d11e4ce6fb0df37214208
Heitor Paulo <heitorino1@gmail.com>
2023-09-25 11:33:48 +0100

    Created project

---
+++ app/usereducer/Todo.tsx
@@ -0,0 +1,14 @@
+export type ITodo = {
+    todoTitle: string
+    complete: boolean
+}
+
+function TodoItem() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TodoItem;
---
+++ app/usereducer/page.tsx
@@ -0,0 +1,17 @@
+function TodoList() {
+    
+    return (
+        <div>
+            <form>
+                <label htmlFor="input">Add a new item</label>
+                <input type="text" name="" id="" />
+            </form>
+            <div>
+                <h1>LIST OF TODOS</h1>
+
+            </div>
+        </div>
+    );
+}
+
+export default TodoList;
