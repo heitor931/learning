bea0d475a5ec7d1ea2ec1683799ed8d60889acba
Heitor Paulo <heitorino1@gmail.com>
2023-10-12 17:27:07 +0100

    Trying to add zod and react hook forms

--- app/models/DataSchema.ts
+++ app/models/DataSchema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod"
 
 export const UserFormSchema = z.object({
-    firstName: z.string().min(2).max(3),
-    lastName: z.string().min(2).max(3),
+    firstName: z.string().min(2).max(30),
+    lastName: z.string().min(2).max(30),
     email: z.string().email(),
     age: z.number().min(18).max(70),
     password: z.string().min(5).max(20),
--- app/zod/Form.tsx
+++ app/zod/Form.tsx
@@ -1,24 +1,31 @@
 "use client"
 import { z } from "zod"
 import { UserFormSchema } from "../models/DataSchema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
 
 type UserForm = z.infer<typeof UserFormSchema>
 function Form() {
-    return (
-        <form className="flex w-96 bg-blue-800 border  shadow-md p-2 rounded flex-col gap-3" action="">
-            <label htmlFor="name">First Name</label>
-            <input className="h-8 rounded  p-1" type="text" name="" id="" />
-            <label className="h-8 rounded  p-1" htmlFor="lastname">Last Name</label>
-            <input className="h-8 rounded  p-1" type="text" name="" id="" />
-            <label htmlFor="email">Email</label>
-            <input className="h-8 rounded  p-1" type="email" name="" id="" />
-            <label htmlFor="age">age</label>
-            <input className="h-8 rounded  p-1" type="number" name="" id="" />
-            <label htmlFor="password">password</label>
-            <input className="h-8 rounded  p-1" type="password" name="" id="" />
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input className="h-8 rounded  p-1" type="password" name="" id="" />
+    const { register, handleSubmit } = useForm<UserForm>({ resolver: zodResolver(UserFormSchema) })
+    const sendDataToDatabse = (data: UserForm) => {
+        console.log("It worked", data);
 
+    }
+    return (
+        <form onSubmit={handleSubmit(sendDataToDatabse)} className="flex w-96 text-black  bg-blue-800 border  shadow-md p-2 rounded flex-col gap-3" action="">
+            <label  className="font-bold" htmlFor="name">First Name</label>
+            <input {...register("firstName")} className="h-8 rounded font-bold  p-1" type="text" name="" />
+            <label   className="h-8 rounded font-bold p-1" htmlFor="lastname">Last Name</label>
+            <input {...register("lastName")} className="h-8 rounded  p-1" type="text" name="" />
+            <label className="font-bold" htmlFor="email">Email</label>
+            <input {...register("email")} className="h-8 rounded  p-1" type="email" name="" />
+            <label className="font-bold" htmlFor="age">age</label>
+            <input {...register("age", { valueAsNumber: true })} className="h-8 rounded  p-1" type="number" name="" />
+            <label className="font-bold" htmlFor="password">password</label>
+            <input {...register("password")} className="h-8 rounded  p-1" type="password" name="" />
+            <label className="font-bold" htmlFor="confirmPassword">Confirm Password</label>
+            <input {...register("confirmPassword")} className="h-8 rounded  p-1" type="password" name="" />
+            <input className="bg-red-700 p-1 rounded text-white" type="submit" value="submit" />
         </form>
     );
 }
